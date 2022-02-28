80759a2071fc389cc7e47c2bc556d9176670c077
Gumato-Roba <gumatorobba@gmail.com>
2022-02-28 17:41:18 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,34 @@
+fun main(){
+    school()
+    statement()
+    fruit()
+    comment()
+
+
+
+}
+fun school(){
+    var name = "akirachix"
+    var x = (name[1].toString()+name[3]+name[4])
+    println(x)
+}
+fun statement(){
+    val y = "my name is Gumato"
+    val z = "and I am 20 years old"
+    println(y+" "+z)
+}
+fun fruit(){
+    var q = "banana"
+    println(q.length)
+}
+fun comment(){
+    val word = "ruforoba"
+    if (word == "ruforoba")
+    println("that's me!")
+    else ("I don't know who that is")
+
+}
+
+
+
+
